0572cccc317e3d7e2fe588e6b38ddaf652853bf6
cdb229 <cdb229@cornell.edu>
2021-03-19 16:31:55 -0400

    updated gui.mli

--- gui.mli
+++ gui.mli
@@ -7,13 +7,15 @@ type t
 (** [init cells] is the GUI for a board of cells, constructed from the
     ground up. A hexagon will be created in every place corresponding to
     a Some value in [cells]. Then, [update_cells cells] will be called. *)
-val init : Cell.t list -> t
+val init : Cell.t option list -> t
 
 (** [update_board gui cells] updates the GUI with all of the data in the
     cells, while leaving any cells that are not in cells. Precondition:
     hexagons of board match the hexagons formed in init *)
-val update_cells : t -> Cell.t list -> t
+val update_cells : t -> Cell.t option list -> t
 
+(** [update_sun gui dir] updates the GUI so that the rays of the sun
+    will appear to point in direction [dir].*)
 val update_sun : t -> HexMap.dir -> t
 
 (* erase the previous render and print the new render to the screen
