a8fbb98dd9d5b25f8eee327856dae15eae8152e3
Caroline Zeng <c.zeng@rrc.local>
2021-03-26 14:38:22 -0400

    Photosynthesis phase (maybe) done

--- board.ml
+++ board.ml
@@ -52,11 +52,63 @@ let place_plant (board : t) cell plant =
 
 (* TODO: need some way to increment life points/decrement light points *)
 
+(** [shadows c1 c2] determines if the [Plant.t] in [c1] would shadow the
+    [Plant.t] in [c2] based on size and distance. *)
+let shadows map c1 c2 =
+  let open Cell in
+  let c1_plnt = plant c1 in
+  let c2_plnt = plant c2 in
+  c2_plnt = Seed
+  ||
+  match c1_plnt with
+  | Seed -> false
+  | Small ->
+      c2_plnt = Small && HexMap.dist map (coord c1) (coord c2) = 1
+  | Medium ->
+      (c2_plnt = Medium || c2_plnt = Small)
+      && HexMap.dist map (coord c1) (coord c2) <= 2
+  | Large ->
+      (c2_plnt = Large || c2_plnt = Medium || c2_plnt = Small)
+      && HexMap.dist map (coord c1) (coord c2) <= 3
+
+let lp_map =
+  let open Plant in
+  function Seed -> 0 | Small -> 1 | Medium -> 2 | Large -> 3
+
+(** [player_lp_helper board player player_cells] returns an updated
+    board with only [player]'s light points updated based on the sun's
+    position. *)
+let player_lp_helper (board : t) player player_cells : t =
+  let update_board = ref board in
+  for i = 0 to List.length player_cells - 1 do
+    let cell = List.nth player_cells i in
+    let fst_neigh = HexMap.neighbor board.map cell board.sun_dir in
+    let snd_neigh = HexMap.neighbor board.map fst_neigh board.sun_dir in
+    let thd_neigh = HexMap.neighbor board.map snd_neigh board.sun_dir in
+    let shadowed =
+      shadows board.map fst_neigh cell
+      || shadows board.map snd_neigh cell
+      || shadows board.map thd_neigh cell
+    in
+    if not shadowed then
+      let lp = lp_map (Cell.plant cell) in
+      let new_plst =
+        List.map
+          (fun ply ->
+            if Player.player_id ply = player then add_lp lp else ply)
+          board.players
+      in
+      update_board := { !update_board with players = new_plst }
+    else ()
+  done;
+  !update_board
+
 (** [lp_helper board player_cells player] returns an updated board with
     each player gaining the appropriate light points based on the sun's
     position. *)
-let lp_helper board =
+let lp_helper (board : t) : t =
   let update_board = ref board in
+  (* let opposite_sun_dir = (board.sun_dir + 3) mod 6 in *)
   for i = 0 to List.length board.players - 1 do
     let player = List.nth board.players i in
     let player_cells =
@@ -68,22 +120,16 @@ let lp_helper board =
               Plant.player_id plant = Player.player_id player)
         (HexMap.flatten board.map)
     in
-    ()
+    update_board := player_lp_helper !update_board player player_cells
   done;
   !update_board
 
-let end_turn board =
-  (* TODO: light points *)
+let end_turn (board : t) : t =
+  (* TODO: In progress: light points *)
   let current_phase = board.current_phase in
   match current_phase with
   | Photosynthesis ->
       { (lp_helper board) with current_phase = Life_Cycle }
-      (* let all_cells = HexMap.flatten board.map in let cur_player =
-         List.nth board.players board.current_turn in let player_cells =
-         List.filter (fun c -> match Cell.plant c with | None -> false |
-         Some plant -> Plant.player_id plant = Player.player_id
-         cur_player) all_cells in { board with current_phase =
-         Life_Cycle } *)
   | Life_Cycle ->
       if board.current_turn = List.length board.players - 1 then
         {
@@ -96,3 +142,5 @@ let end_turn board =
       else { board with current_turn = board.current_turn + 1 }
 
 let sun_dir board = board.sun_dir
+
+let flat_board (board : t) : Cell.t list = HexMap.flatten board.map
