fd70758c006007a07acaabd952781be8df944531
cdb229 <cdb229@cornell.edu>
2021-03-25 21:54:36 -0400

    main.ml integrates with HexMap interface

--- main.ml
+++ main.ml
@@ -21,7 +21,7 @@ let soil_cell3 = Cell.init_cell 3 None
 
 let soil_cell4 = Cell.init_cell 4 None
 
-let main () =
+let main1 () =
   let gui =
     init_gui
       [
@@ -43,4 +43,22 @@ let main () =
   |> update_cursor ANSITerminal.Red (Some { diag = 0; col = 0 })
   |> render
 
-let () = main ()
+let main2 () =
+  let hex_map = HexMap.init_map in
+  let gui = init_gui (HexMap.flatten hex_map) in
+  gui
+  |> update_cells
+       [
+         basic_cell1 { diag = 6; col = 6 };
+         basic_cell2 { diag = 4; col = 2 };
+         basic_cell3 { diag = 0; col = 3 };
+         basic_cell4 { diag = 2; col = 4 };
+         basic_cell1 { diag = 5; col = 3 };
+         basic_cell2 { diag = 1; col = 3 };
+         basic_cell3 { diag = 3; col = 3 };
+         basic_cell4 { diag = 0; col = 0 };
+       ]
+  |> update_cursor ANSITerminal.Red (Some { diag = 2; col = 2 })
+  |> render
+
+let () = main2 ()
