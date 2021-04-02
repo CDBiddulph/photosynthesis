9757addbeefa38dabeb78d7ee1010dd51e981dff
Caroline Zeng <c.zeng@rrc.local>
2021-04-01 23:08:11 -0400

    Add authors files and install with time estimate

---
+++ authors.mli
@@ -0,0 +1,8 @@
+(** CS 3110 Spring 2021 Project MS1
+
+    @author Adrian Jung (akj47)
+    @author Caleb Biddulph (cdb229)
+    @author Caroline Zeng (chz6) *)
+
+(** [hours_worked] is the number of hours you worked on this assignment. *)
+val hours_worked : int
