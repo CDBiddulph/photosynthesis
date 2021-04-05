94a23ec0a6704e1ccca310a012159f73dbcab24d
cdb229 <cdb229@cornell.edu>
2021-04-05 16:53:22 -0400

    Created placeholder store.ml

---
+++ store.ml
@@ -0,0 +1,19 @@
+type t = unit
+
+exception InsufficientLightPoints of int
+
+exception FullOfPlant of Plant.plant_stage
+
+let init_store = failwith "Not Implemented"
+
+let buy_plant store stage light_points = failwith "Not Implemented"
+
+let add_plant store stage = failwith "Not Implemented"
+
+let cost store stage = failwith "Not Implemented"
+
+let cost_at_n store stage n = failwith "Not Implemented"
+
+let num_remaining store stage = failwith "Not Implemented"
+
+let capacity store stage = failwith "Not Implemented"
