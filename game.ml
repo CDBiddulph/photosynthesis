0c63da5c243652ecd4cb8a1a6b7789b6fb33708d
cdb229 <cdb229@cornell.edu>
2021-04-05 20:03:04 -0400

    Added more functions to game.ml

--- game.ml
+++ game.ml
@@ -1,5 +1,5 @@
 type t = {
-  players : Player.t list;
+  players : (PlayerId.t * Player.t) list;
   board : Board.t;
 }
 
@@ -12,6 +12,24 @@ let init_game num_players ruleset =
     | _ -> failwith "Must be 2-4 players"
   in
   {
-    players = List.map Player.init_player player_ids;
+    players =
+      List.map (fun id -> (id, Player.init_player id)) player_ids;
     board = Board.init_board ruleset;
   }
+
+let player_of game id = List.assoc id game.players
+
+let update_board game board = { game with board }
+
+let update_players game players = { game with players }
+
+let place_plant game coord plant =
+  update_board game (Board.place_plant game.board coord plant)
+
+let harvest game player_id coord =
+  update_board game (Board.harvest game.board player_id coord)
+
+let buy_plant game player_id stage = failwith "Not Implemented"
+
+let is_place_plant_legal game coord plant =
+  Board.is_place_plant_legal game.board coord plant
